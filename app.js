3c8ff0521eb59dc88bc6c94db4805371e0db07da
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-07-05 13:37:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+//PARA EXECUTAR ISSO NO TERMINAL NODE, basta ir no caminho do arquivo no CMD e digitar> node app.js
+
+//console.log('Ola mundo');
+
+
+/*var pessoa = {
+    nome: "Arthur",
+    idade: 20
+}
+
+console.log(pessoa);
+*/
+
+/*function somar(a, b){
+    return a+b;
+}
+
+console.log(somar(10,5));
+*/
+/*
+var cursos = function(){
+    console.log('teste teste');
+}
+
+cursos();
+setTimeout(cursos, 5000);
+ */
+/*
+function organizar(numero){
+    console.log('Ordem do cliente:', numero);
+    entrega(function(){
+        console.log("Ordem de entrega:",numero);
+    })
+}
+
+function entrega(callback){
+    setTimeout(callback, 5000);
+}
+
+organizar(1);
+organizar(2);
+organizar(3);
+organizar(4);
+ */
+
